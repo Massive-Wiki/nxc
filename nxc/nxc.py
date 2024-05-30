31644f74b4a869fea4ed43b3031ce1916860d6f7
Willliam L. Anderson <band@users.noreply.github.com>
2024-05-30 15:53:52 -0500

    use subparsers to separate init and build parsing

--- nxc/nxc.py
+++ nxc/nxc.py
@@ -5,6 +5,12 @@ import os
 import shutil
 import logging
 from pathlib import Path
+import yaml
+
+# load config file                                                                                                          
+def load_config(path):
+    with open(path) as infile:
+        return yaml.safe_load(infile)
 
 def init_site(directory):
     # Check and handle the specified directory
@@ -32,37 +38,48 @@ def init_site(directory):
         logging.error(f"Failed to initialize project: {e}")
     return
 
-def build_site(site_dir, output_dir):
+def build_site(args):
+    site_dir = args.input
+    output_dir = args.output
     logging.info('build this website from %s to %s', site_dir, output_dir)
+    logging.info(f"config file: {site_dir}{args.config}")
+    logging.info(f"templates: {site_dir}{args.templates}")
     return
 
 def main():
-    # Setup logger
+    # setup logger
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
     
-    # Setup argument parser
-    parser = argparse.ArgumentParser(description='Initialize project directory with starter files.')
-    parser.add_argument('command', choices=['init', 'build'], help='Command to execute')
-    parser.add_argument('-d','--directory', metavar='DIR', type=str, help='Directory to process')
-    parser.add_argument('-o', '--output', help='output directory')
+    # setup argument parsers
+    parser = argparse.ArgumentParser(description='Initialize or build website for a collection of Markdown files.')
+    subparsers = parser.add_subparsers(required=True)
+    # subparser for "init" command
+    parser_init = subparsers.add_parser('init')
+    parser_init.add_argument('-d','--directory', metavar='DIR', type=str, required=True, help='Directory to initialize.')
+    parser_init.set_defaults(command='init')
+    # subparser for "build" command
+    parser_build = subparsers.add_parser('build')
+    parser_build.add_argument('-i', '--input', required=True, help='directory of Markdown files')
+    parser_build.add_argument('-o', '--output', required=True, help='output directory')
+    parser_build.add_argument('--config', '-c', default='/.massivewikibuilder/nxc.yaml', help='path to YAML config file')
+    parser_build.add_argument('--templates', '-t', default='/.massivewikibuilder/this-wiki-themes/dolce', help='directory for HTML templates')
+    parser_build.add_argument('--lunr', action='store_true', help='include this to create lunr index (requires npm and lunr to be\
+ installed, read docs)')
+    parser_build.add_argument('--commits', action='store_true', help='include this to read Git commit messages and times, for All\
+ Pages')
+ 
+    parser_build.set_defaults(command='build')
     
     args = parser.parse_args()
     logging.info(args)
 
-    # Validate and process arguments
     match args.command:
         case 'init':
-            if not args.directory:
-                parser.error("Please specify the directory to initialize with --directory")
-                return
             print(f'Initializing in directory: {args.directory}')
-            init_site(args.directory)
+#            init_site(args.directory)
         case 'build':
-            if not args.directory or not args.output:
-                parser.error('Both --directory and --output are required for "build" command')
-                return
-            print(f'Building from directory: {args.directory} to output: {args.output}')
-            build_site(args.directory, args.output)
+            print(f'Building from input directory: {args.input} to output: {args.output}')
+            build_site(args)
         case _:
             return
 
